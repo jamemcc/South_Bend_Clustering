aed79e709b2c715e60da26bb761e19fa7dc65081
jkosteck <42282134+jkosteck@users.noreply.github.com>
2019-02-18 17:54:44 -0600

    More EDA

--- Data Gathering-Zillow Static.R
+++ Data Gathering-Zillow Static.R
@@ -1,17 +1,12 @@
 homeValueIndex <- read.csv("Zillow Home Value Index - All Homes.csv",
                            stringsAsFactors = F)
+
 homeValueIndex <- homeValueIndex %>%
   filter(State == "IN") %>%
   filter(CountyName == "Saint Joseph County")
 
-tidyValueIndex <- gather(homeValueIndex, key = DateString, value = "HomeValue",
+longValueIndex <- gather(homeValueIndex, key = DateString, value = "HomeValue",
                          -c(RegionID, RegionName, City, State, Metro, CountyName, SizeRank))
-
-tidyValueIndex <- 3
-
-str_sub(tidyValueIndex$DateString[1], start = 2)
-as.Date("1996.04.01", "%Y.%m.%d")
-
 
 neighborhoodRent <- read.csv("Neighborhood Rent Prices-Zillow.csv",
                              stringsAsFactors = F) %>%
@@ -30,7 +25,16 @@ tidyRentZillow <- longRentZillow %>%
   mutate(DateString = str_sub(DateString, start = 2)) %>%
   mutate(Date = as.Date(paste0(DateString, ".1"), "%Y.%m.%d") + months(1) - days(1))
 
+tidyValueIndex <- longValueIndex %>%
+  mutate(DateString = str_sub(DateString, start = 2)) %>%
+  mutate(Date = as.Date(paste0(DateString, ".1"), "%Y.%m.%d") + months(1) - days(1))
+
 ggplot(
   data = tidyRentZillow,
   aes(x = Date, y = ZRI, color = RegionName)
+) + geom_point() + scale_y_continuous(labels = dollar_format())
+
+ggplot(
+  data = tidyValueIndex,
+  aes(x = Date, y = HomeValue, color = RegionName)
 ) + geom_point() + scale_y_continuous(labels = dollar_format())
