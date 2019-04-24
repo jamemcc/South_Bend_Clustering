f3d660403de8f6a0e623b74345c6e959c084a12d
jkosteck <42282134+jkosteck@users.noreply.github.com>
2019-04-23 20:42:30 -0500

    Leaving only necessary files

--- Data Gathering-South Bend API.R
+++
@@ -1,7 +0,0 @@
-library(rjson)
-
-json_file <- "https://opendata.arcgis.com/datasets/2636929ba1b945b28965af3762da4af2_1.geojson"
-
-json_data <- fromJSON(file = json_file)
-
-data.frame(unlist(json_data$features[[1]]$properties))
--- Data Gathering-South Bend Static.R
+++
@@ -1,8 +0,0 @@
-abandondedProperties <- read.csv("Abandoned_Property_Parcels.csv", stringsAsFactors = F)
-
-crimeStats <- read.csv("South Bend Crime Stats.csv", stringsAsFactors = F)
-
-chronicProperties <- read.csv("Chronic_Problem_Properties.csv", stringsAsFactors = )
-
-shootings <- read.csv("Criminally_Assaulted_Shootings.csv", stringsAsFactors = F)
-
